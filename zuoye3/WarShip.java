98f94468fc482ea1ea766c6a78ddb322c6b898e7
waq502775999 <wuanqi99@qq.com>
2015-07-08 21:20:49 +0800

    第三天作业

---
+++ zuoye3/WarShip.java
@@ -0,0 +1,16 @@
+package zuoye3;
+
+public class WarShip extends Weapon{
+
+
+	public WarShip() {
+		// TODO Auto-generated constructor stub
+	}
+	public void attack(){
+		System.out.println("水雷");
+	}
+    public void move(){
+    	System.out.println("在水里游");
+    }
+
+}
